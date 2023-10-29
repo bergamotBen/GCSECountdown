e833c4be1d95ceaa841f978313aed5b6eb43fa71
bergamotBen <bergamotBen@icloud.com>
2023-10-29 18:14:33 +0000

    deleted unused files

--- js/script.js
+++ js/script.js
@@ -1,24 +1,56 @@
-countdown = (stamp) => {
+const countdown = (stamp) => {
   const currentTime = Date.now();
   const duration = stamp - currentTime;
-  const msArray = [604800000, 86400000, 3600000, 60000, 1000];
-  const durations = [];
-  const intervals = ["weeks", "days", "hours", "minutes", "seconds"];
-  const intervalsToDisplay = [];
+  const msArray = [604800000, 86400000, 3600000, 60000];
+  const intervals = ["weeks", "days", "hours", "minutes"];
+  const finalArr = [];
   let remainder = duration;
 
-  for (let i = 0; i < 5; i++) {
-    if (Math.floor(remainder / msArray[i]) === 1) {
-      intervalsToDisplay.push(intervals[i].slice(0, -1));
-      durations.push(Math.floor(remainder / msArray[i]));
+  for (let i = 0; i < 4; i++) {
+    if (Math.floor(remainder / msArray[i]) === 0) {
+      finalArr.push("");
+    } else if (Math.floor(remainder / msArray[i]) === 1) {
+      finalArr.push(
+        ` ${Math.floor(remainder / msArray[i])} ${intervals[i].slice(0, -1)},`
+      );
     } else {
-      intervalsToDisplay.push(intervals[i]);
-      durations.push(Math.floor(remainder / msArray[i]));
+      finalArr.push(` ${Math.floor(remainder / msArray[i])} ${intervals[i]},`);
     }
     let next = remainder % msArray[i];
     remainder = next;
   }
-  return `${durations[0]} ${intervalsToDisplay[0]}, ${durations[1]} ${intervalsToDisplay[1]}, ${durations[2]} ${intervalsToDisplay[2]}, ${durations[3]} ${intervalsToDisplay[3]}, ${durations[4]} ${intervalsToDisplay[4]}`;
+  return finalArr.join("").slice(0, -1);
 };
 
-countdown(1718006400000);
+const countdownText = document.querySelector("#stamp1");
+countdownText.innerText = countdown(1716451200000);
+setInterval(() => {
+  countdownText.innerText = countdown(1716451200000);
+}, 60000);
+
+const countdownText2 = document.querySelector("#stamp2");
+countdownText2.innerText = countdown(1717660800000);
+setInterval(() => {
+  countdownText.innerText = countdown(1717660800000);
+}, 60000);
+
+const purple = [199, 123, 219];
+const pink = [237, 82, 121];
+let currentColour = [199, 123, 219];
+
+const background = document.querySelector(".bg");
+background.style.backgroundColor = `rgb(${currentColour[0]}, ${currentColour[1]}, ${currentColour[2]})`;
+
+const scroll = document.getElementById("wrapper");
+scroll.addEventListener("scroll", () => {
+  const scrollHeight = 2973;
+  const percentage = scroll.scrollTop / scrollHeight;
+  const rRange = pink[0] - purple[0];
+  const gRange = purple[1] - pink[1];
+  const bRange = purple[2] - pink[2];
+
+  currentColour[0] = Math.round(percentage * rRange + purple[0]);
+  currentColour[1] = Math.round(1 - percentage * gRange + purple[1]);
+  currentColour[2] = Math.round(1 - percentage * bRange + purple[2]);
+  background.style.backgroundColor = `rgb(${currentColour[0]}, ${currentColour[1]}, ${currentColour[2]})`;
+});
